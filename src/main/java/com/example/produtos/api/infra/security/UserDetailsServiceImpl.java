877b22fdbc00360a0174394457094d6569ff4617
Vagner Pinto <professor.vagnerpinto@gmail.com>
2022-08-16 13:39:46 -0300

    chore: encoding proprierties file

--- src/main/java/com/example/produtos/api/infra/security/UserDetailsServiceImpl.java
+++ src/main/java/com/example/produtos/api/infra/security/UserDetailsServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.produtos.api.infra.security;
 
-import com.example.produtos.api.users.User;
 import com.example.produtos.api.users.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
