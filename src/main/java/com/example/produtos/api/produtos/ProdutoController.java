b60ea78efcba917327f4a7f21d89c05c503d3669
Vagner Pinto <professor.vagnerpinto@gmail.com>
2022-08-16 15:26:47 -0300

    docs: swagger config

--- src/main/java/com/example/produtos/api/produtos/ProdutoController.java
+++ src/main/java/com/example/produtos/api/produtos/ProdutoController.java
@@ -1,5 +1,7 @@
 package com.example.produtos.api.produtos;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -11,24 +13,28 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/produtos")
+@Api(value = "Produtos")
 public class ProdutoController {
 
     @Autowired
     private ProdutoService service;
 
     @GetMapping
+    @ApiOperation(value = "Retorna todos os produtos cadastrados.")
     public ResponseEntity<List<ProdutoDTO>> selectAll() {
         List<ProdutoDTO> produtos = service.getProdutos();
         return ResponseEntity.ok(produtos);
     }
 
     @GetMapping("{id}")
+    @ApiOperation(value = "Retorna um produto pelo campo identificador.")
     public ResponseEntity<ProdutoDTO> selectById(@PathVariable("id") Long id) {
         ProdutoDTO produto = service.getProdutoById(id);
         return ResponseEntity.ok(produto);
     }
 
     @GetMapping("/nome/{nome}")
+    @ApiOperation(value = "Retorna uma lista de produtos pela chave nome.")
     public ResponseEntity<List<ProdutoDTO>> selectByNome(@PathVariable("nome") String nome) {
         List<ProdutoDTO> produtos = service.getProdutosByNome(nome);
         return produtos.isEmpty() ?
@@ -38,6 +44,7 @@ public class ProdutoController {
 
     @PostMapping
     @Secured({"ROLE_ADMIN"})
+    @ApiOperation(value = "Insere um novo produto.")
     public ResponseEntity<String> insert(@RequestBody Produto produto){
         ProdutoDTO p = service.insert(produto);
         URI location = getUri(p.getId());
@@ -45,6 +52,7 @@ public class ProdutoController {
     }
 
     @PutMapping("{id}")
+    @ApiOperation(value = "Altera um produto existente.")
     public ResponseEntity<ProdutoDTO> update(@PathVariable("id") Long id, @RequestBody Produto produto){
         produto.setId(id);
         ProdutoDTO p = service.update(produto, id);
@@ -54,6 +62,7 @@ public class ProdutoController {
     }
 
     @DeleteMapping("{id}")
+    @ApiOperation(value = "Deleta um produto.")
     public ResponseEntity<String> delete(@PathVariable("id") Long id){
         service.delete(id);
         return ResponseEntity.ok().build();
--- src/main/java/com/example/produtos/api/users/UserController.java
+++ src/main/java/com/example/produtos/api/users/UserController.java
@@ -1,5 +1,7 @@
 package com.example.produtos.api.users;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,17 +13,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@Api(value = "Produtos")
 public class UserController {
     @Autowired
     private UserService service;
 
     @GetMapping()
+    @ApiOperation(value = "Retorna todos usuários cadastrados.")
     public ResponseEntity<List<UserDTO>> get() {
         List<UserDTO> list = service.getUsers();
         return ResponseEntity.ok(list);
     }
 
     @GetMapping("/info")
+    @ApiOperation(value = "Retorna os detalhes do usuário logado.")
     public UserDTO userInfo(@AuthenticationPrincipal User user) { //a anotação retorna o user logado
 
         //User userLoged = (User) JwtUtil.getUserDetails(); //outra forma de retornar o user logado (nesse projeto)
