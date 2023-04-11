b05cfc850605b9dc2295069a9faffa506e357247
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2023-04-11 15:45:55 -0300

    fix: data.sql to order role

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,11 +1,11 @@
 insert into role(id,nome) values (1, 'ROLE_USER');
 insert into role(id,nome) values (2, 'ROLE_ADMIN');
 
-insert into user(id,nome,email,login,senha) values (1,'Admin','admin@gmail.com','admin','$2a$10$HKveMsPlst41Ie2LQgpijO691lUtZ8cLfcliAO1DD9TtZxEpaEoJe');
-insert into user(id,nome,email,login,senha) values (2,'User','user@gmail.com','user','$2a$10$HKveMsPlst41Ie2LQgpijO691lUtZ8cLfcliAO1DD9TtZxEpaEoJe');
+insert into user(id,nome,email,login,senha) values (1,'User','user@gmail.com','user','$2a$10$HKveMsPlst41Ie2LQgpijO691lUtZ8cLfcliAO1DD9TtZxEpaEoJe');
+insert into user(id,nome,email,login,senha) values (2,'Admin','admin@gmail.com','admin','$2a$10$HKveMsPlst41Ie2LQgpijO691lUtZ8cLfcliAO1DD9TtZxEpaEoJe');
 
-insert into user_roles(user_id,role_id) values(1, 2);
-insert into user_roles(user_id,role_id) values(2, 1);
+insert into user_roles(user_id,role_id) values(1, 1);
+insert into user_roles(user_id,role_id) values(2, 2);
 
 INSERT INTO produto (id, nome, valor, descricao, situacao, estoque) VALUES
 (1, 'Café', 12.90, 'Café em pó tradicional Igaçu lata 400g', 1, 100),
