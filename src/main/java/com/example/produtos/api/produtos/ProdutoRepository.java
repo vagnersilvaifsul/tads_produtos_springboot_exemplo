16669b6400829bb23c6cd603a5513c5ce5da62b0
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2023-04-26 14:09:54 -0300

    fix: findByName with like

--- src/main/java/com/example/produtos/api/produtos/ProdutoRepository.java
+++ src/main/java/com/example/produtos/api/produtos/ProdutoRepository.java
@@ -1,10 +1,12 @@
 package com.example.produtos.api.produtos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ProdutoRepository extends JpaRepository<Produto,Long> {
 
+    @Query(value = "SELECT p FROM Produto p where p.nome like ?1")
     List<Produto> findByNome(String nome);
 }
--- src/main/java/com/example/produtos/api/produtos/ProdutoService.java
+++ src/main/java/com/example/produtos/api/produtos/ProdutoService.java
@@ -25,7 +25,7 @@ public class ProdutoService {
     }
 
     public List<ProdutoDTO> getProdutosByNome(String nome) {
-        return rep.findByNome(nome).stream().map(ProdutoDTO::create).collect(Collectors.toList());
+        return rep.findByNome(nome+"%").stream().map(ProdutoDTO::create).collect(Collectors.toList());
     }
 
     public ProdutoDTO insert(Produto produto) {
