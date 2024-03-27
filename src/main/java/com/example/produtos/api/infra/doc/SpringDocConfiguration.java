c67010d2a975392ebd909a675bd2ca1055e7756b
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2024-03-27 18:25:29 -0300

    feat: documentação com springdoc. Adicionado o authorization global.

--- src/main/java/com/example/produtos/api/infra/doc/SpringDocConfiguration.java
+++ src/main/java/com/example/produtos/api/infra/doc/SpringDocConfiguration.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,13 +14,16 @@ import org.springframework.context.annotation.Configuration;
 public class SpringDocConfiguration {
     @Bean
     public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+            .addSecurityItem(new SecurityRequirement()
+                .addList(securitySchemeName))
             .components(new Components()
-                .addSecuritySchemes("bearer-key",
-                    new SecurityScheme()
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT")))
+                .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                    .name(securitySchemeName)
+                    .type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")))
             .info(new Info()
                 .title("API de Exemplo Desenvolvida para as Aulas de TADS")
                 .description("API Rest da aplicação tads_produtos_springboot_exemplo, contendo as funcionalidades de CRUD de produtos.")
