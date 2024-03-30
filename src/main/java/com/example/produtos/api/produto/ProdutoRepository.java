950bc04827b44c8f9e4746bb3b3b6ba1364a62f3
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2024-03-30 15:14:34 -0300

    docs: didática do projeto

--- src/main/java/com/example/produtos/api/produto/ProdutoRepository.java
+++ src/main/java/com/example/produtos/api/produto/ProdutoRepository.java
@@ -7,15 +7,17 @@ import java.util.List;
 
 /*
   A grande vantagem do Padrão Repository reside no fato de ele permitir montar consultas pelo padrão de nome do método
-  (e de trazer o CRUD pronto, sem precisar escrever uma linha de código).
-  Note que ele utiliza o padrão Domain Speak na busca dos dados (e deixa para o JPA aplicar o padrão Query Speak).
+  (e, claro, de trazer o CRUD pronto, sem precisar escrever uma linha de código).
+  Um repositório reúne, essencialmente, todas as operações de dados para um determinado tipo de domínio em um só lugar.
+  O aplicativo se comunica com o repositório em Domain Speak, e o repositório, por sua vez, se comunica com o armazenamento
+  de dados em Query Speak.
  */
 public interface ProdutoRepository extends JpaRepository<Produto,Long> {
     /*
         O Padrão Repository do Spring Data, apesar de nos fornecer uma inferface Domain Speak, inteligente o
         suficiente para montar o SQL da consulta para nós, é flexível o bastante para nos permitir utilizar as
         nossas próprias Query Speak (é o caso do SQL apresentado na anotação @Query). Ou seja, o Spring Data nos
-        permite alternar entre Domain Speak e Query Speak rapidamente.
+        permite alternar entre Domain Speak e Query Speak rapidamente. É muita flexibilidade!
      */
     //Exemplo de aplicação de Query Speak no Padrão Repository
     @Query(value = "SELECT p FROM Produto p where p.nome like ?1 order by p.nome")
--- src/main/java/com/example/produtos/api/usuarios/UsuarioRepository.java
+++ src/main/java/com/example/produtos/api/usuarios/UsuarioRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 /*
   A grande vantagem do Padrão Repository reside no fato de ele permitir montar consultas pelo padrão de nome do método
-  (e de trazer o CRUD pronto, sem precisar escrever uma linha de código).
-  Note que ele utiliza o padrão Domain Speak na busca dos dados (e deixa para o JPA aplicar o padrão Query Speak).
+  (e, claro, de trazer o CRUD pronto, sem precisar escrever uma linha de código).
+  Um repositório reúne, essencialmente, todas as operações de dados para um determinado tipo de domínio em um só lugar.
+  O aplicativo se comunica com o repositório em Domain Speak, e o repositório, por sua vez, se comunica com o armazenamento
+  de dados em Query Speak.
  */
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
     Usuario findByUsuario(String usuario);
