a0ecc83443073f89dc2b5129002208a0bd2e89e1
vagnersilvaifsul <vagnersilva@ifsul.edu.br>
2024-05-01 19:38:05 -0300

    feat: send mail

--- src/main/java/com/example/produtos/api/usuario/validacoes/ValidacaoLoginDoUsuario.java
+++ src/main/java/com/example/produtos/api/usuario/validacoes/ValidacaoLoginDoUsuario.java
@@ -1,7 +1,6 @@
 package com.example.produtos.api.usuario.validacoes;
 
 import com.example.produtos.api.autenticacao.UsuarioDTO;
-import com.example.produtos.api.usuario.Usuario;
 
 public interface ValidacaoLoginDoUsuario {
     void validar(UsuarioDTO usuarioDTO);
